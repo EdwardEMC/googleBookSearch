6d813fe6dd6c87a7b019b0045b5046651eded36e
EMC <edwardcoad.ec@gmail.com>
2020-04-12 09:29:38 +0930

    Api routes in place, server updated to connect to mongoose

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Serve up static assets (usually on heroku)
+const PORT = process.env.PORT || 3001;
+
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBookSearch")
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
