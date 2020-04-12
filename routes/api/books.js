d95f3ed68ef92368a365b250dae115b827c4283e
EMC <edwardcoad.ec@gmail.com>
2020-04-12 09:54:02 +0930

    controller and model added

---
+++ routes/api/books.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const searchController = require("../../controllers/searchController");
+
+router.route("/")
+  .get(searchController.findAll)
+  .post(searchController.create);
+
+router.route("/:id")
+  .get(searchController.findById)
+  .put(searchController.update)
+  .delete(searchController);
+
+module.exports = router;
